05a085836a1cad0846dc177a80c1678deac29014
noveltaker <pnw123456@icloud.com>
2022-07-10 11:19:37 +0900

    chore: @Component 를 통한 Custom Naming

--- src/main/kotlin/com/example/base/config/base/item/MyItem.kt
+++ src/main/kotlin/com/example/base/config/base/item/MyItem.kt
@@ -1,10 +1,8 @@
 package com.example.base.config.base.item
 
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 
-@Component
-@Qualifier("myItem")
+@Component("myItem")
 class MyItem : Item {
     override fun getName(): String = "my_item"
 }
