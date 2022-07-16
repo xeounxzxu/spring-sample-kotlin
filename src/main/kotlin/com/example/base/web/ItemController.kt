f2250def98021d820222929bf4d4ca3a66cddfaa
noveltaker <pnw123456@icloud.com>
2022-07-16 11:15:03 +0900

    feat: 다국어 처리시 파라미터 처리 예제

--- src/main/kotlin/com/example/base/web/ItemController.kt
+++ src/main/kotlin/com/example/base/web/ItemController.kt
@@ -1,18 +1,24 @@
 package com.example.base.web
 
+import com.example.base.enums.MsgType
 import com.example.base.service.ItemService
+import com.example.base.utils.I18nMsgUtil
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ItemController constructor(
-    private val itemService: ItemService
+    private val itemService: ItemService,
+    private val i18nMsgUtil: I18nMsgUtil
 ) {
 
-    @GetMapping("/test")
-    fun test() {
+    @GetMapping("/message")
+    fun getMessage() {
         throw NullPointerException()
     }
+
+    @GetMapping("message2")
+    fun getMessage(name: String) = i18nMsgUtil.getMessage("test", arrayOf(name))
 
     @GetMapping("/api/hello")
     fun showByString(): String = "hello"
