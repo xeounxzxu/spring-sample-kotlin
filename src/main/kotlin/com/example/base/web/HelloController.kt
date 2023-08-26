26df14609b95f8d077eee26a673d4ebdaa4353a7
spark <pnw123456@icloud.com>
2023-08-26 11:08:46 +0900

    feat: custom handermapping add

---
+++ src/main/kotlin/com/example/base/web/HelloController.kt
@@ -0,0 +1,18 @@
+package com.example.base.web
+
+import com.example.base.core.extents.PublicAPI
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+data class HelloDto(val kr: String = "안녕", val en: String = "hello")
+
+@RestController
+@RequestMapping("v1")
+class HelloController {
+
+    @PublicAPI
+    @GetMapping("hello")
+    fun getHello(): HelloDto = HelloDto()
+}
+
