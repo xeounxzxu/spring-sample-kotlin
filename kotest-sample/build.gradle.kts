a822b88e58bf59994f24e6b59ff86c2e977fd5b5
Black <pnw123456@icloud.com>
2024-01-26 23:16:46 +0900

    feat: kotest dependencies add

--- kotest-sample/build.gradle.kts
+++ kotest-sample/build.gradle.kts
@@ -5,3 +5,12 @@ tasks.getByName("bootJar") {
 tasks.getByName("jar") {
     enabled = false
 }
+
+
+dependencies {
+
+    // kotest dependencies add
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.1.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.1.0")
+    implementation("io.kotest:kotest-extensions-spring:4.4.3")
+}
