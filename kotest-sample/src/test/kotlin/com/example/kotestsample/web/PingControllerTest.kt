191b8dd14148bcf7eed33a4019aaae368e87e1c6
Black <pnw123456@icloud.com>
2024-02-09 22:59:38 +0900

    feat: update kotest

--- kotest-sample/src/test/kotlin/com/example/kotestsample/web/PingControllerTest.kt
+++ kotest-sample/src/test/kotlin/com/example/kotestsample/web/PingControllerTest.kt
@@ -1,6 +1,7 @@
 package com.example.kotestsample.web
 
 import com.example.kotestsample.extensions.RestDocExtension
+import com.example.kotestsample.extensions.manualRestDocumentation
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.kotest.assertions.print.print
 import io.kotest.core.spec.style.FunSpec
@@ -8,20 +9,18 @@ import io.kotest.extensions.spring.SpringExtension
 import io.restassured.http.ContentType
 import io.restassured.module.mockmvc.RestAssuredMockMvc
 import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
-import org.springframework.restdocs.ManualRestDocumentation
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
 import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.restdocs.payload.PayloadDocumentation
+import org.springframework.restdocs.payload.PayloadDocumentation.responseFields
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder
 
 class PingControllerTest : FunSpec() {
-
-    lateinit var mockMvc: MockMvcRequestSpecification
-
-    private val restDocumentation = ManualRestDocumentation()
 
     private val converter = MappingJackson2HttpMessageConverter(ObjectMapper())
 
@@ -31,33 +30,11 @@ class PingControllerTest : FunSpec() {
 
         extensions(SpringExtension, RestDocExtension())
 
-        beforeTest {
-
-            val localMockMvc =
-                MockMvcBuilders.standaloneSetup(controller)
-                    .apply<StandaloneMockMvcBuilder>(
-                        MockMvcRestDocumentation.documentationConfiguration(
-                            this.restDocumentation
-                        )
-                    )
-                    .setMessageConverters(converter)
-                    .build()
-
-            // todo : 요거 설정 안하면 ,,, 에러 발새
-            this.restDocumentation.beforeTest(PingController::class.java, it.name.testName)
-
-            mockMvc = RestAssuredMockMvc
-                .given()
-                .mockMvc(localMockMvc)
-        }
-
-        afterSpec {
-            this.restDocumentation.afterTest();
-        }
+        beforeTest {  }
 
         test("test") {
 
-            mockMvc
+            call(controller, converter)
                 .contentType(ContentType.JSON)
                 .get("/api/ping")
                 .then()
@@ -65,7 +42,7 @@ class PingControllerTest : FunSpec() {
                 .apply(
                     document(
                         "{class-name}/{method-name}",
-                        PayloadDocumentation.responseFields(
+                        responseFields(
                             PayloadDocumentation.fieldWithPath("status").type(JsonFieldType.STRING).description("상태")
                         )
                     )
@@ -73,8 +50,26 @@ class PingControllerTest : FunSpec() {
                 .expect {
                     it.print()
                 }
-
         }
     }
 }
+
+suspend fun call(controller: Any, converter: AbstractJackson2HttpMessageConverter): MockMvcRequestSpecification {
+
+    val localMockMvc =
+        MockMvcBuilders.standaloneSetup(controller)
+            .apply<StandaloneMockMvcBuilder>(
+                MockMvcRestDocumentation.documentationConfiguration(
+                    manualRestDocumentation()
+                )
+            )
+            .setMessageConverters(converter)
+            .alwaysDo<StandaloneMockMvcBuilder>(MockMvcResultHandlers.print())
+            .build()
+
+    return RestAssuredMockMvc
+        .given()
+        .mockMvc(localMockMvc)
+}
+
 
