05c279ee17811cf11e78cb46b5d5ffd8267443ef
Black <pnw123456@icloud.com>
2024-02-11 12:04:01 +0900

    refactor: thread config update

--- virtual-thread/src/main/kotlin/com/example/base/config/ThreadConfiguration.kt
+++ virtual-thread/src/main/kotlin/com/example/base/config/ThreadConfiguration.kt
@@ -4,13 +4,14 @@ import org.apache.coyote.ProtocolHandler
 import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.core.task.AsyncTaskExecutor
 import org.springframework.core.task.support.TaskExecutorAdapter
 import java.util.concurrent.Executors
 
-
-@Configuration
+/**
+ * todo : spring 3.x.x 이하일때 사용
+ */
+// @Configuration
 class ThreadConfiguration {
 
     @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
