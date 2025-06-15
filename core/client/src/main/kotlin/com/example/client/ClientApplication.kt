0968134862291e82a0b088877bbb3f9ccc491347
xeounxzxu <pnw123456@icloud.com>
2025-06-15 15:12:43 +0900

    fix: open class update

--- core/client/src/main/kotlin/com/example/client/ClientApplication.kt
+++ core/client/src/main/kotlin/com/example/client/ClientApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ClientApplication
+open class ClientApplication
 
 fun main(args: Array<String>) {
     runApplication<ClientApplication>(*args)
